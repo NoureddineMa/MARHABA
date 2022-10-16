72b25b5d9f6c36f7c9c475350baaaf584ab541f2
NoureddineMa <noureddinemaher13@gmail.com>
2022-10-16 17:02:33 +0100

    login (+hashing password) and register for Users  whit JWT  and middelware to protect route /getUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,11 +9,23 @@ const User = require('../models/authModel')
 // @Route :api/auth/login
 // *** acces : public ***
 const Login = asyncHandler(async (req,res) => {
-    try {
-        res.status(200).send(req.body)
-    } catch (error) {
-        res.send(error)
-    }
+     const {email,password} = req.body
+     // check for user email :
+     const user = await User.findOne({email})
+    
+     if(user && (await bcyrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            adresse: user.adresse,
+            token: generateToken(user._id)
+        })
+     } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+     }
 })
 
 
@@ -23,11 +35,61 @@ const Login = asyncHandler(async (req,res) => {
 const Register =  asyncHandler(async (req,res) => {
     const {name,email,password,phone,adresse } = req.body
 
-    if(!name || !email || !password || !phone || !adresse ) {
+    if(!name  || !email || !password || !phone || !adresse ) {
         res.status(400)
         throw new Error ('Please add all fields')
     }
-    res.json({message : 'register'}) 
+
+
+    // check UserifExists:
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already Exist')
+    }
+
+    // hadsh Password : "Bcryptjs"
+    const salt = await bcyrypt.genSalt(10)
+    const hashedPassword = await bcyrypt.hash(password, salt)
+
+    // create a user : 
+    const user = await User.create ({
+        name,
+        email,
+        password: hashedPassword,
+        phone,
+        adresse,
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            password: user.password,
+            email: user.email,
+            phone: user.phone,
+            adresse: user.adresse,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
+})
+
+
+// *** *** *** method :post *** *** ***
+// @Route :api/auth/getUser
+// *** acces : public ***
+const Getme = asyncHandler(async (req,res) => {
+    const{_id, name, email} = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 })
 
 
@@ -56,4 +118,13 @@ const ResetPassword =  (req,res) => {
 }
 
 
-module.exports = { Login, Register, ForgetPassword, ResetPassword }
+
+// generate JWT 
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '3d',
+    })
+}
+
+
+module.exports = { Login, Register, ForgetPassword, ResetPassword , Getme }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const { Login , Register , ForgetPassword , ResetPassword } = require('../controllers/authcontroller')
+const { Login , Register , ForgetPassword , ResetPassword , Getme } = require('../controllers/authcontroller')
+
+const {protect} = require('../middlewares/authMiddelware')
 
 router.post('/login',Login)
 router.post('/register',Register)
 router.post('/forgetpassword',ForgetPassword)
 // => :token to reset password (just for test Route)
 router.post('/resetpassword',ResetPassword) 
+router.post('/getme', protect,Getme)
 
 
 module.exports = { router }
